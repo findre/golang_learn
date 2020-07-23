06fc9e19a69534c315b7cd5c7ef3615b385c0ce9
johnnyleaf <fxtafreeman@163.com>
2020-07-23 10:29:40 +0800

    "many language hello world, TDD"

--- hello/hello.go
+++ hello/hello.go
@@ -2,16 +2,33 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
-func Hello(name string) string {
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
+
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "world"
 	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(language) + name
 }
 
 func main() {
-	s := Hello("world")
+	s := Hello("world", "")
 	fmt.Printf("%s", s)
 }
